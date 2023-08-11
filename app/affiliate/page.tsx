cf8eed24e52fbb1efcb9776bb33aeb2e3b3785fb
Sintiasilaen <sintiasilaen67@gmail.com>
2023-08-11 09:21:37 +0700

    update

---
+++ app/affiliate/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const affiliate = () => {
+  return (
+    <div className="min-h-screen justify-center items-center text-center bg-blue-400 bg-opacity-10">
+      <h1 className="text-5xl font-bold">Afiliasi Cloudbank</h1>
+      <span className="text-xl text-gray-600">
+        Program afiliasi hosting Indonesia dengan komisi hingga 75%. Daftarkan
+        diri Anda GRATIS sekarang juga dan dapatkan penghasilan pasif hingga
+        jutaan rupiah!
+      </span>
+    </div>
+  );
+};
+
+export default affiliate;
