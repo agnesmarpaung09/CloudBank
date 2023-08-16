75ba11a520dbdad2ae1d030d5c38b79ae13abd69
esphiaphelinahutabarat <70991491+esphiaphelinahutabarat@users.noreply.github.com>
2023-08-16 11:31:32 +0700

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,27 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
+ 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridAutoRows : {
+
+        
+      },
+      backgroundImage: {
+        "icon-test": "url('/images/BG LAYER 1.png')"
+      }
+
+    
+
+
+      // backgroundImage: {
+      //   'image-agenda': "url('/image/PTS SMK.png')",
+      // }
+    },
   },
   plugins: [],
-};
+}
