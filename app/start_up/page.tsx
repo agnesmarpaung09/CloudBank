910fc24626f327c4f362cc781f10c5804a2c02b8
puritampubolon08 <70991197+puritampubolon08@users.noreply.github.com>
2023-08-19 12:03:02 +0700

    Update

--- app/start_up/page.tsx
+++ app/start_up/page.tsx
@@ -36,7 +36,7 @@ const StartUp = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 md:gap-4 ml-10">
         <div className="w-80">
           <div className="border-r-4 h-20 mt-16 "></div>
-          <h1 className="font-semibold text-sm text-center -mt-24 font-serif mr-6">
+          <h1 className="font-semibold text-sm text-center -mt-24 mr-6">
             Performa Yang Terbaik
           </h1>
           <h2 className="mt-2 text-xs text-justify mr-10">
@@ -46,7 +46,7 @@ const StartUp = () => {
         </div>
         <div className="w-72">
         <div className="border-r-4 h-20 mt-16"></div>
-          <h1 className="font-semibold text-sm text-center -mt-24 font-serif">
+          <h1 className="font-semibold text-sm text-center -mt-24">
             Keamanan Backup
           </h1>
           <h2 className="mt-2 text-xs text-center mr-4">
@@ -54,7 +54,7 @@ const StartUp = () => {
           </h2>
         </div>
         <div className="w-72">
-          <h1 className="font-semibold text-sm text-center mt-12 font-serif">
+          <h1 className="font-semibold text-sm text-center mt-12">
             Dedicated Support
           </h1>
           <h2 className="mt-2 text-xs text-justify mb-8">
