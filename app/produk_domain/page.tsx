6d25df04af641ef9dbdfd651863ba75affae3e71
esphiaphelinahutabarat <70991491+esphiaphelinahutabarat@users.noreply.github.com>
2023-08-22 16:58:14 +0700

    update

--- app/produk_domain/page.tsx
+++ app/produk_domain/page.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const produk_domain = () => {
   const kotakData = [
-    { imageSrc: "images/kotak1.png", title: ".art", buttonText: "Beli" },
-    { imageSrc: "images/kotak2.png", title: ".asia", buttonText: "Beli" },
-    { imageSrc: "images/kotak3.png", title: ".com", buttonText: "Beli" },
-    { imageSrc: "images/kotak4.png", title: ".photo", buttonText: "Beli" },
-    { imageSrc: "images/kotak4.png", title: ".photo", buttonText: "Beli" },
+    { imageSrc: "images/kotak1.png", title: ".art", buttonText: "Beli", text: "Harga Mulai Dari", price : " Rp.206.000/tahun " },
+    { imageSrc: "images/kotak2.png", title: ".asia", buttonText: "Beli", text: "Harga Mulai Dari", price : " Rp.233.500/tahun "},
+    { imageSrc: "images/kotak3.png", title: ".com", buttonText: "Beli", text: "Harga Mulai Dari", price : " Rp.154.500/tahun " },
+    { imageSrc: "images/kotak4.png", title: ".photo", buttonText: "Beli", text: "Harga Mulai Dari", price : " Rp.360.000/tahun " },
+    { imageSrc: "images/kotak4.png", title: ".photo", buttonText: "Beli", text: "Harga Mulai Dari", price : " Rp.164.500/tahun " },
   ];
 
   return (
@@ -48,25 +48,20 @@ const produk_domain = () => {
 </div>
 
 
+<div className="mx-auto max-w-5xl grid mb-44 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-56 text-justify justify-center ml-16 mt-">
+  {kotakData.map((item, index) => (
+    <div key={index} className="h-64 w-48 relative hover:bg-white ml-3">
+      <div className="relative h-full w-full flex items-center">
+        <img src="images/kotak.png" alt="Kotak" className="h-full w-full" />
+        <p className="absolute top-2 left-0 w-full text-center text-gray-800 font-bold text-4xl">{item.title}</p>
+        <p className="absolute mt-0 left-0 w-full text-center text-gray-400 text-sm">{item.text}</p>
+        <p className="absolute mt-24 left-0 w-full text-center text-blue-700 font-bold text-sm">{item.price}</p>
+        <button className="absolute bottom-0 mt-4 left-0 w-full bg-orange-500 text-white py-1 px-2 text-center hover:bg-blue-600 focus:outline-none">{item.buttonText}</button>
+      </div>
+    </div>
+  ))}
+</div>
 
-
-
-        <div className="mx-auto max-w-5xl grid mb-44 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-56 text-justify justify-center ml-16 mt-">
-          {kotakData.map((item, index) => (
-         <div key={index} className="h-64 w-48 relative hover:bg-white ml-3">
-         <div className="relative h-full w-full flex items-center">
-           <img
-             src="images/kotak.png"
-             alt="Kotak"
-             className="h-full w-full"
-           />
-           <p className="absolute top-2 left-0 w-full text-center text-gray-800 font-bold text-4xl">{item.title}</p>
-           <button className="absolute bottom-0 mt-4 left-0 w-full bg-orange-500 text-white py-1 px-2 text-center hover:bg-blue-600 focus:outline-none">{item.buttonText}</button>
-         </div>
-       </div>
-       
-          ))}  
-        </div>
 
    <div className="flex items-start">
   <div className="relative h-96 w-72 mx-72">
